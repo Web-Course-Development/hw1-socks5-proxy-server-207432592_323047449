b528e735aecc0537bfa3830082b1779a9c09b83f
amitp3171 <amitp3171@gmail.com>
2026-05-27 18:02:17 +0300

    added first function negotiateAuth

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
@@ -39,4 +41,51 @@ func handleConnection(conn net.Conn) {
 	// 4. Connect to target server
 	// 5. Send success/error reply
 	// 6. Relay data between client and target
+
+	_, err := negotiateAuth(conn)
+	if err != nil {
+		log.Printf("negotiateAuth error: %v", err)
+		return
+	}
+}
+
+func negotiateAuth(conn net.Conn) (byte, error) {
+	header := make([]byte, 2)
+	if _, err := io.ReadFull(conn, header); err != nil {
+		return 0, fmt.Errorf("reading greeting header: %w", err)
+	}
+	
+	if header[0] != 0x05 {
+		return 0, fmt.Errorf("unexpected SOCKS version: %d", header[0])
+	}
+
+	nMethods := int(header[1])
+ 	methods := make([]byte, nMethods)
+	if _, err := io.ReadFull(conn, methods); err != nil {
+		return 0, fmt.Errorf("reading methods: %w", err)
+	}
+
+ 	proxyUser := os.Getenv("PROXY_USER")
+	requireAuth := proxyUser != ""
+	selected := byte(0xFF)
+	for _, m := range methods {
+		if requireAuth && m == 0x02 {
+			selected = 0x02
+			break
+		}
+		if !requireAuth && m == 0x00 {
+			selected = 0x00
+			break
+		}
+	}
+ 
+	if _, err := conn.Write([]byte{0x05, selected}); err != nil {
+		return 0, fmt.Errorf("writing method selection: %w", err)
+	}
+
+	if selected == 0xFF {
+		return 0, fmt.Errorf("no acceptable auth method offered by client")
+	}
+
+	return selected, nil
 }
